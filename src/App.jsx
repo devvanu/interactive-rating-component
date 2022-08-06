a8973f58830e70f9b9dfb085dc073e0fa778ec1b
Anu Kumari <anukumari21529@gmail.com>
2022-08-07 00:09:38 +0530

    finished the challenge

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import { useState } from 'react';
+
+import RatingContainer from './components/Rating/RatingContainer';
+import ThankYou from './components/ThankYou/ThankYou';
 import './App.css';
 
 const App = () => {
+
+  const [ratingPoint, setRatingPoint] = useState(0);
+  
+  const ratingPointHandler = (point) => {
+    setRatingPoint(point);
+  }
+
   return (
-    <div>App</div>
+    <div className='app'>
+      { ratingPoint == 0  && 
+        <RatingContainer ratingPoint={ ratingPointHandler } />}
+      { ratingPoint > 0 && <ThankYou ratingPoint={ratingPoint} />}
+    </div>
   )
 }
 
